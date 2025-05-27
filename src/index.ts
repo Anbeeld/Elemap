2f94dd5f05cbd0e9450cec7b00c8998d022f2763
Anbeeld <anbeeld@gmail.com>
2025-05-27 12:48:44 +0200

    Generic index class based on map type arg

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,64 @@ import HexagonMap from "./hexagon/map.js";
 import { CustomTileStyleDecls, CustomSchema, modifyGridMapStyleSchema } from "./style/schema.js";
 import { CustomConfig, validateConfig } from "./config.js";
 import { MapType, setProperty } from "./utils.js";
-import { AbstractGridMap, AbstractMap } from "./map.js";
 import { AbstractTile } from "./tile.js";
 
-export default class Elemap {
-  private _: AbstractMap;
+type MapTypeStrings = `${MapType}`;
 
-  constructor(type?: MapType, config?: CustomConfig, schema?: CustomSchema) {
+type ElemapType<M> = 
+  M extends "rectangle" ? RectangleMap :
+  M extends "hexagon" ? HexagonMap :
+  never;
+
+export default class Elemap<M extends MapTypeStrings = `${MapType.Rectangle}`> {
+  private _: ElemapType<M>;
+
+  constructor(type?: M, config?: CustomConfig, schema?: CustomSchema) {
     let validatedType = type === MapType.Rectangle ? MapType.Rectangle : (type === MapType.Hexagon ? MapType.Hexagon : MapType.Rectangle);
     let validatedConfig = validateConfig(config || {});
     let validatedSchema = modifyGridMapStyleSchema(schema || {});
 
     if (validatedType === MapType.Rectangle) {
-      this._ = new RectangleMap(validatedConfig, validatedSchema);
+      this._ = new RectangleMap(validatedConfig, validatedSchema) as ElemapType<M>;
     } else if (validatedType === MapType.Hexagon) {
-      this._ = new HexagonMap(validatedConfig, validatedSchema);
+      this._ = new HexagonMap(validatedConfig, validatedSchema) as ElemapType<M>;
     }
 
-    setProperty(this, 'export', () : any => {
-      return this._.export();
-    });
+    // For JavaScript - shield method names from mangling
+    this.shield__export();
+    this.shield__render();
+    this.shield__tileByIndex();
+  }
 
-    setProperty(this, 'render', (container: HTMLElement) : void => {
-      this._.render(container);
-    });
+  public export() {
+    return this.method__export();
+  }
+  private shield__export() {
+    setProperty(this, 'export', () => this.method__export());
+  }
+  private method__export() {
+    return this._.export();
+  }
+  
+  public render(container: HTMLElement) { this.method__render(container); }
+  private shield__render() {
+    setProperty(this, 'render', (container: HTMLElement) => this.method__render(container));
+  }
+  private method__render(container: HTMLElement) : void {
+    this._.render(container);
+  }
 
-    setProperty(this, 'tileByIndex', (i: number, j: number) : ElemapTile|undefined => {
-      let tile = (this._ as AbstractGridMap).grid.tileByIndex(i, j);
-      if (tile) {
-        return new ElemapTile(tile);
-      }
-      return undefined;
-    });
+  public tileByIndex(i: number, j: number) { return this.method__tileByIndex(i, j); }
+  private shield__tileByIndex() {
+    setProperty(this, 'tileByIndex', (i: number, j: number) => this.method__tileByIndex(i, j));
+  }
+  private method__tileByIndex(i: number, j: number) : ElemapTile|undefined {
+    // @ts-ignore
+    let tile = this._.grid.tileByIndex(i, j);
+    if (tile) {
+      return new ElemapTile(tile);
+    }
+    return undefined;
   }
 }
 
@@ -44,8 +70,17 @@ class ElemapTile {
   constructor(map: AbstractTile) {
     this._ = map;
 
-    setProperty(this, 'updateStyle', (decls: CustomTileStyleDecls, replace: boolean = false) : void => {
-      this._.updateStyle(decls, replace);
-    });
+    // For JavaScript - shield method names from mangling
+    this.shield__updateStyle();
+  }
+
+  public updateStyle(decls: CustomTileStyleDecls, replace: boolean = false) : void {
+    this.method__updateStyle(decls, replace);
+  }
+  private shield__updateStyle() {
+    setProperty(this, 'updateStyle', (decls: CustomTileStyleDecls, replace: boolean = false) => this.method__updateStyle(decls, replace));
+  }
+  private method__updateStyle(decls: CustomTileStyleDecls, replace: boolean = false) : void {
+    this._.updateStyle(decls, replace);
   }
 }
