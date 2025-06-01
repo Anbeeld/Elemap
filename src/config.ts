31d5392b7411d4446d3cc202e7d04de743e60476
Anbeeld <anbeeld@gmail.com>
2025-06-01 17:23:02 +0200

    Decluttered configToGridMapArguments

--- src/config.ts
+++ src/config.ts
@@ -6,18 +6,20 @@ import { GridOrientation, GridOffset, DeepPartial } from "./utils.js";
 export type Config = DeepPartial<GridMapArguments>;
 
 export function configToGridMapArguments(config: Config) : GridMapArguments {
+  let configSize = mangleProperty(config, 'size'),
+      configGrid = mangleProperty(config, 'grid');
   return {
     ids: mangleMapIds(mangleProperty(config, 'ids')),
     grid: {
-      ids: mangleGridIds(mangleProperty(mangleProperty(config, 'grid'), 'ids')),
+      ids: mangleGridIds(mangleProperty(configGrid, 'ids')),
       size: {
-        width: mangleProperty(mangleProperty(config, 'size'), 'width') || 32,
-        height: mangleProperty(mangleProperty(config, 'size'), 'height') || 18
+        width: mangleProperty(configSize, 'width') || 32,
+        height: mangleProperty(configSize, 'height') || 18
       },
-      orientation: mangleProperty(mangleProperty(config, 'grid'), 'orientation') || GridOrientation.Pointy,
-      offset: mangleProperty(mangleProperty(config, 'grid'), 'offset') || GridOffset.Odd,
-      schema: mangleGridStyleSchema(mangleProperty(mangleProperty(config, 'grid'), 'schema')),
-      tiles: mangleProperty(mangleProperty(config, 'grid'), 'tiles')
+      orientation: mangleProperty(configGrid, 'orientation') || GridOrientation.Pointy,
+      offset: mangleProperty(configGrid, 'offset') || GridOffset.Odd,
+      schema: mangleGridStyleSchema(mangleProperty(configGrid, 'schema')),
+      tiles: mangleProperty(configGrid, 'tiles')
     },
     schema: modifyGridMapStyleSchema(mangleGridMapStyleSchema(mangleProperty(config, 'schema')) || {})
   };
