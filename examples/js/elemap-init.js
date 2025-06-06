594bbec3ba0c4a7dadcd0d606a3473f8bf0b2507
Anbeeld <anbeeld@gmail.com>
2025-06-06 15:55:01 +0200

    Updated examples with new args and method setup

--- examples/js/elemap-init.js
+++ examples/js/elemap-init.js
@@ -1,11 +1,10 @@
 const elemap = new Elemap(
   typeof type !== 'undefined' ? type : 'rectangle',
-  typeof config !== 'undefined' ? config : {},
-  typeof schema !== 'undefined' ? schema : {}
+  typeof config !== 'undefined' ? config : {}
 );
 
-elemap.tileByIndex(1, 1).updateStyle({outer: 'background-color: red;'});
-elemap.tileByIndex(1, 2).updateStyle({outer: 'background-color: green;'});
-elemap.tileByIndex(2, 2).updateStyle({outer: 'background-color: blue;'});
+elemap.grid.tileByIndex(1, 1).updateStyle({outer: 'background-color: red;'});
+elemap.grid.tileByIndex(1, 2).updateStyle({outer: 'background-color: green;'});
+elemap.grid.tileByIndex(2, 2).updateStyle({outer: 'background-color: blue;'});
 
 elemap.render(document.getElementById('elemap-container'));
