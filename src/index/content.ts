8ece01a26b4e61298d6ae3bf235d4ae652c43945
Anbeeld <anbeeld@gmail.com>
2025-09-12 18:16:26 +0200

    Mutate and report in index content class

--- src/index/content.ts
+++ src/index/content.ts
@@ -1,10 +1,35 @@
+import { Mutation } from '../utils.js';
 import { Content } from '../content.js';
+import { demangleProperty } from '../mangle.js';
 
 export class ElemapContent {
   private _: Content;
 
   constructor(content: Content) {
     this._ = content;
-    this._;
+
+    // For JavaScript - ensure methods are available by their original names
+    this.demangle__mutate();
+    this.demangle__report();
+  }
+  
+  public mutate(mutation: Mutation) : void {
+    return this.method__mutate(mutation);
+  }
+  private demangle__mutate() {
+    demangleProperty(this, 'mutate', (mutation: Mutation) => this.method__mutate(mutation));
+  }
+  private method__mutate(mutation: Mutation) {
+    return this._.mutate(mutation);
+  }
+
+  public report() {
+    return this.method__report();
+  }
+  private demangle__report() {
+    demangleProperty(this, 'report', () => this.method__report());
+  }
+  private method__report() {
+    return this._.report();
   }
 }
