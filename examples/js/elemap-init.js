d28bea396e1ecfdc8049560ceb83b46656a77310
Anbeeld <anbeeld@gmail.com>
2025-09-12 18:28:01 +0200

    Updated example init to work with new version

--- examples/js/elemap-init.js
+++ examples/js/elemap-init.js
@@ -3,8 +3,10 @@ const elemap = new Elemap(
   typeof config !== 'undefined' ? config : {}
 );
 
-elemap.grid.tileByCoords(1, 1).updateStyle({outer: 'background-color: red;'});
-elemap.grid.tileByCoords(1, 2).updateStyle({outer: 'background-color: green;'});
-elemap.grid.tileByCoords(2, 2).updateStyle({outer: 'background-color: blue;'});
+elemap.grid.createTiles({width: 32, height: 18}, {x: 0, y: 0});
+
+elemap.grid.tileByCoords([1, 1]).updateStyle({outer: 'background-color: red;'});
+elemap.grid.tileByCoords([2, 1]).updateStyle({outer: 'background-color: green;'});
+elemap.grid.tileByCoords([2, 2]).updateStyle({outer: 'background-color: blue;'});
 
 elemap.render(document.getElementById('elemap-container'));
